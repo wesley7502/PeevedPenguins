9ed626e4d0da81eaa57b910bb1e77516369ed6a3
Wesley Tsai <wesley7502@gmail.com>
2016-06-27 09:51:57 -0700

    Completed Peeved Penguins
    The basic

---
+++ MainScene.swift
@@ -0,0 +1,37 @@
+import SpriteKit
+
+class MainScene: SKScene {
+    
+    /* UI Connections */
+    var buttonPlay: MSButtonNode!
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        
+        /* Set UI connections */
+        buttonPlay = self.childNodeWithName("buttonPlay") as! MSButtonNode
+        
+        /* Setup restart button selection handler */
+        buttonPlay.selectedHandler = {
+            
+            /* Grab reference to our SpriteKit view */
+            let skView = self.view as SKView!
+            
+            /* Load Game scene */
+            let scene = GameScene(fileNamed:"GameScene") as GameScene!
+            
+            /* Ensure correct aspect mode */
+            scene.scaleMode = .AspectFit
+            
+            /* Show debug */
+            skView.showsPhysics = true
+            skView.showsDrawCount = true
+            skView.showsFPS = true
+            
+            /* Start game scene */
+            skView.presentScene(scene)
+        }
+        
+    }
+    
+}
